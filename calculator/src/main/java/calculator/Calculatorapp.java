d4c7b6d7fbdf4486e1c341c048266ff0de9bb3c7
Uyisam <uyisamaboobakkar@gmail.com>
2026-01-21 12:23:31 +0530

    .

--- calculator/src/main/java/calculator/Calculatorapp.java
+++ calculator/src/main/java/calculator/Calculatorapp.java
@@ -1,5 +1,18 @@
 package calculator;
+import java.util.Scanner;
 
 public class Calculatorapp {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter (ex: 5 + 2): ");
+        
+        double a = s.nextDouble();
+        char op = s.next().charAt(0);
+        double b = s.nextDouble();
 
+        double res = (op == '+') ? (a + b) : (op == '-') ? (a - b) : 
+                     (op == '*') ? (a * b) : (op == '/') ? (a / b) : 0;
+
+        System.out.println("Result: " + res);
+    }
 }
